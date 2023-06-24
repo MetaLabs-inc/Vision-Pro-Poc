82a7cec34d80dba54210bd4670a520eb2fad0f59
Marco Fiorito <marcofiorito1@gmail.com>
2023-06-24 16:23:03 -0300

    Initial Commit

---
+++ VisionProHelloWorld/VisionProHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionProHelloWorldApp.swift
+//  VisionProHelloWorld
+//
+//  Created by Marco Fiorito on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionProHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
